07bcc8b81eb1d67ae49eddd8ecd4127cd5c8e0eb
Anastasia Shatokhina <shnatya@gmail.com>
2021-07-19 22:51:13 -0700

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+let newHeader = document.createElement('h1');
+newHeader.id = "victory";
+
+document.body.appendChild(newHeader);
+/*newHeader.textContent = "Anastasia is the champion";*/
+victory.innerHTML = "<h1>Anastasia is the champion</h1>";
+
+const mainElement = document.querySelector('main#main');
+mainElement.remove();
+
